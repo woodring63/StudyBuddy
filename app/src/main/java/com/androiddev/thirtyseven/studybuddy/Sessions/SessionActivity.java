c326f81f659d6e121cc2c8f9c08e2d6a0df7a340
Joseph Elliott <joey.w.elliott@gmail.com>
2016-02-24 11:31:27 -0600

    Added TODO for view pager

--- app/src/main/java/com/androiddev/thirtyseven/studybuddy/Sessions/SessionActivity.java
+++ app/src/main/java/com/androiddev/thirtyseven/studybuddy/Sessions/SessionActivity.java
@@ -16,6 +16,7 @@ public class SessionActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session);
+        // TODO add tab view
     }
 
     @Override

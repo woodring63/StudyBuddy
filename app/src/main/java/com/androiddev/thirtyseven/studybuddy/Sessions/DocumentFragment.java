9334764a5bccb4cef46bd104a667cbbf9f8e81eb
Joseph Elliott <joey.w.elliott@gmail.com>
2016-02-28 22:03:25 -0600

    Added tabs, barely works

---
+++ app/src/main/java/com/androiddev/thirtyseven/studybuddy/Sessions/DocumentFragment.java
@@ -0,0 +1,21 @@
+package com.androiddev.thirtyseven.studybuddy.Sessions;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.androiddev.thirtyseven.studybuddy.R;
+
+/**
+ * Created by Joseph Elliott on 2/28/2016.
+ */
+public class DocumentFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_whiteboard, container, false);
+    }
+
+}
--- app/src/main/java/com/androiddev/thirtyseven/studybuddy/Sessions/SessionActivity.java
+++ app/src/main/java/com/androiddev/thirtyseven/studybuddy/Sessions/SessionActivity.java
@@ -1,7 +1,12 @@
 package com.androiddev.thirtyseven.studybuddy.Sessions;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -10,13 +15,23 @@ import com.androiddev.thirtyseven.studybuddy.R;
 /**
  * Created by Joseph Elliott on 2/2/2016.
  */
-public class SessionActivity extends Activity {
+public class SessionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session);
-        // TODO add tab view
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Document"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tasks"));
+        tabLayout.addTab(tabLayout.newTab().setText("Whiteboard"));
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.session_pager);
+        final SessionPagerAdapter adapter = new SessionPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
     }
 
     @Override

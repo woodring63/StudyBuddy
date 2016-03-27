c8fa7c4cec96b06960a712eae7f297831e323296
Joseph Elliott <joey.w.elliott@gmail.com>
2016-03-26 23:07:07 -0500

    added some task views

--- app/src/main/java/com/androiddev/thirtyseven/studybuddy/Sessions/Tasks/TaskFragment.java
+++ app/src/main/java/com/androiddev/thirtyseven/studybuddy/Sessions/Tasks/TaskFragment.java
@@ -5,8 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.androiddev.thirtyseven.studybuddy.R;
+import com.androiddev.thirtyseven.studybuddy.Sessions.Whiteboard.WhiteboardView;
 
 /**
  * Created by Joseph Elliott on 2/28/2016.
@@ -15,7 +17,22 @@ public class TaskFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tasks, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_tasks, container, false);
+
+        try {
+
+        } catch (NullPointerException e) {
+            // rip in pieces
+        }
+
+        return rootView;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstances) {
+        super.onCreate(savedInstances);
+        // other things
     }
 
 }

ca0d7eb907d3b7385b24b7d1f960cfb653e8c255
Nathan Gilbert <nlg@iastate.edu>
2016-03-28 13:20:04 -0500

    Save to drive function worksgit add app/src/main/java/com/androiddev/thirtyseven/studybuddy/Sessions/DocumentFragment.java

--- app/src/main/java/com/androiddev/thirtyseven/studybuddy/Sessions/DocumentFragment.java
+++ app/src/main/java/com/androiddev/thirtyseven/studybuddy/Sessions/DocumentFragment.java
@@ -3,6 +3,8 @@ package com.androiddev.thirtyseven.studybuddy.Sessions;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -19,12 +21,12 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.drive.Drive;
 import com.google.android.gms.drive.DriveApi;
-import com.google.android.gms.drive.DriveContents;
-import com.google.android.gms.drive.DriveId;
 import com.google.android.gms.drive.MetadataChangeSet;
 
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 /**
  * Created by Joseph Elliott on 2/28/2016.
@@ -32,16 +34,17 @@ import java.io.IOException;
 public class DocumentFragment extends Fragment
         implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
-    EditText text;
-    GoogleApiClient mGoogleApiClient;
-    DriveId driveId;
-    Activity activity; // For use in nested objects
+    private EditText text;
+    private GoogleApiClient mGoogleApiClient;
+    private Activity activity; // Used to pass a context
+    //private SharedPreferences prefs;
 
     static final String TAG = "DocumentFragment";
+    private static final int REQUEST_CODE_SAVE = 1;
+    private static final int REQUEST_CODE_RESOLUTION = 2;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Log.d(TAG, "onCreateView");
 
         View view = inflater.inflate(R.layout.fragment_document, container, false);
         text = (EditText) view.findViewById(R.id.drive_file_text_display);
@@ -49,18 +52,20 @@ public class DocumentFragment extends Fragment
         Button clearButton = (Button) view.findViewById(R.id.clear_button);
         Button saveButton = (Button) view.findViewById(R.id.save_button);
 
+        //prefs = PreferenceManager.getDefaultSharedPreferences(activity);
+
         activity = getActivity();
 
         mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
                 .addApi(Drive.API)
                 .addScope(Drive.SCOPE_FILE)
+                //.setAccountName(prefs.getString("email", "None"))
                 .setAccountName("nlg@iastate.edu")
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .build();
         mGoogleApiClient.connect();
 
-        Log.d(TAG, "mGoogleApiClient created");
         loadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -106,45 +111,63 @@ public class DocumentFragment extends Fragment
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (mGoogleApiClient.isConnecting()) {
+                    Toast.makeText(activity,
+                            "Study Buddy is still connecting to Google Drive. Please wait.",
+                            Toast.LENGTH_LONG).show();
+                } else if (!mGoogleApiClient.isConnected()) {
+                    Toast.makeText(activity,
+                            "Study Buddy is not connected to Google Drive. Please try again later.",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    // Make new contents to save to the drive
+                    Drive.DriveApi.newDriveContents(mGoogleApiClient)
+                            .setResultCallback(new ResultCallback<DriveApi.DriveContentsResult>() {
+                                @Override
+                                public void onResult(DriveApi.DriveContentsResult driveContentsResult) {
+                                    // If unsuccessful
+                                    if (!driveContentsResult.getStatus().isSuccess()) {
+                                        Toast.makeText(activity,
+                                                "Could not create new drive contents. Please try again.",
+                                                Toast.LENGTH_LONG).show();
+                                    }
+                                    // Otherwise continue
+                                    else {
+                                        // Set contents
+                                        try {
+                                            FileOutputStream fileOutputStream = new FileOutputStream(driveContentsResult
+                                                    .getDriveContents()
+                                                    .getParcelFileDescriptor()
+                                                    .getFileDescriptor());
+                                            Writer writer = new OutputStreamWriter(fileOutputStream);
+                                            writer.write(text.getText().toString());
+                                            writer.close();
+                                            fileOutputStream.close();
+                                        } catch (IOException e) {
+                                            Log.e(TAG, e.toString());
+                                        }
 
-                // Make new contents to save to the drive
-                Drive.DriveApi.newDriveContents(mGoogleApiClient)
-                        .setResultCallback(new ResultCallback<DriveApi.DriveContentsResult>() {
-                    @Override
-                    public void onResult(DriveApi.DriveContentsResult driveContentsResult) {
-                        // If unsuccessful
-                        if (!driveContentsResult.getStatus().isSuccess()) {
-                            Toast.makeText(activity,
-                                    "Could not create new drive contents. Please try again.",
-                                    Toast.LENGTH_LONG).show();
-                        }
-                        // Otherwise continue
-                        else {
-                            // Set contents
-                            try {
-                            FileWriter writer = new FileWriter(driveContentsResult
-                                    .getDriveContents()
-                                    .getParcelFileDescriptor()
-                                    .getFileDescriptor());
-                                writer.write(text.getText().toString());
-                            } catch (IOException e) {
-                                // rip in pieces
-                            }
+                                        // Create the MetadataChangeSet for file info
+                                        MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
+                                                .setTitle("Session Document.txt")
+                                                .setMimeType("text/plain")
+                                                .build();
 
-                            // Create the MetadataChangeSet for file info
-                            MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
-                                    .setTitle("Session Document.txt")
-                                    .setMimeType("text/plain")
-                                    .build();
-
-                            driveId = Drive.DriveApi.getRootFolder(mGoogleApiClient)
-                                    .createFile(mGoogleApiClient,
-                                            changeSet,
-                                            driveContentsResult.getDriveContents())
-                                    .await().getDriveFile().getDriveId();
-                        }
-                    }
-                });
+                                        IntentSender intentSender = Drive.DriveApi
+                                                .newCreateFileActivityBuilder()
+                                                .setInitialMetadata(changeSet)
+                                                .setInitialDriveContents(driveContentsResult.getDriveContents())
+                                                .build(mGoogleApiClient);
+                                        try {
+                                            activity.startIntentSenderForResult(
+                                                    intentSender, REQUEST_CODE_SAVE, null, 0, 0, 0);
+                                        } catch (IntentSender.SendIntentException e) {
+                                            Log.e(TAG, e.toString());
+                                        }
+                                    }
+                                }
+                            });
+                }
             }
         });
 
@@ -152,8 +175,37 @@ public class DocumentFragment extends Fragment
     }
 
     @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_CODE_SAVE:
+                if (resultCode == activity.RESULT_OK) {
+                    Log.d(TAG, "File save successful");
+                    Toast.makeText(activity, "File saved!", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Log.d(TAG, "File save unsuccessful. Result Code: " + resultCode);
+                    Toast.makeText(activity, "Save failed. Please try again.", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case REQUEST_CODE_RESOLUTION:
+                // Sign-in failed
+                if (!(resultCode == activity.RESULT_OK)) {
+                    Toast.makeText(activity, "Sing-in failed. Please try again later.", Toast.LENGTH_LONG).show();
+                }
+                // Sign-in successful, retry connection
+                else {
+                    mGoogleApiClient.connect();
+                }
+                break;
+            default:
+                Log.d(TAG, "Request Code not found");
+                break;
+        }
+    }
+
+    @Override
     public void onConnected(Bundle bundle) {
-        Toast.makeText(activity, "Connection successful!", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "Google Drive connection successful");
     }
 
     // Handles when the connection goes down
@@ -172,7 +224,18 @@ public class DocumentFragment extends Fragment
     // Handles a failed connection
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-        Toast.makeText(activity, "Connection failed. Please try again later.", Toast.LENGTH_LONG).show();
+        // If there is a resolution, try to resolve it
+        if (connectionResult.hasResolution()) {
+            try {
+                connectionResult.startResolutionForResult(activity, REQUEST_CODE_RESOLUTION);
+            }
+            catch (IntentSender.SendIntentException e) {
+                Log.e(TAG, e.toString());
+            }
+        }
+        else {
+            Toast.makeText(activity, "Google Drive connection failed. Please try again later.", Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
@@ -197,4 +260,6 @@ public class DocumentFragment extends Fragment
         }
         super.onResume();
     }
+
+
 }
